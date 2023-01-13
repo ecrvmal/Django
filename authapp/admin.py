fee32946d0294fa3377a8ca5651d9e5df6caf2cb
Vladimir Malchevskiy <102606951+ecrvmal@users.noreply.github.com>
2023-01-14 00:57:49 +0300

    Lesson 2 (#1)
    * Preconfig
    
    * lesson_2 Hello world with CBV and FBV
    
    * lesson_2 Static site with Django
    
    * lesson_2 Code style fix

--- authapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from authapp import models
-
-
-@admin.register(models.CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ["id", "username", "email", "is_active", "date_joined"]
-    ordering = ["-date_joined"]
--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,9 @@
-import debug_toolbar
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("i18n/", include("django.conf.urls.i18n")),
     path("", RedirectView.as_view(url="mainapp/")),
-    path("social_auth/", include("social_django.urls", namespace="social")),
-    path("mainapp/", include("mainapp.urls", namespace="mainapp")),
-    path("authapp/", include("authapp.urls", namespace="authapp")),
+    path("mainapp/", include("mainapp.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
