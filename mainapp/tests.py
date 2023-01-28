92dd8523d6c7765e2942aa519de6454bd034dcbb
Vladimir M <ecrvmal@yandex.ru>
2023-01-28 14:28:55 +0300

    testing problems

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,8 +1,7 @@
 import pickle
 from http import HTTPStatus
-
 from unittest import mock
-from authapp import models as authapp_models
+
 from django.conf import settings
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.core import mail as django_mail
@@ -12,9 +11,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+
+from authapp import models as authapp_models
 from mainapp import models as mainapp_models
 from mainapp import tasks as mainapp_tasks
-
 
 
 class TestMainPage(TestCase):
@@ -36,7 +36,6 @@ class TestNewsPage(TestCase):
         self.user_admin = authapp_models.CustomUser.objects.get(username="admin")
         self.client_with_auth.force_login(self.user_admin, backend="django.contrib.auth.backends.ModelBackend")
 
-    
     def test_page_open_list(self):
         path = reverse("mainapp:news")
         result = self.client.get(path)
@@ -116,12 +115,11 @@ class TestNewsPage(TestCase):
 
 class TestTaskMailSend(TestCase):
     fixtures = ("authapp/fixtures/001_user_admin.json",)
+
     def test_mail_send(self):
         message_text = "test_message_text"
         user_obj = authapp_models.CustomUser.objects.first()
-        mainapp_tasks.send_feedback_mail(
-            {"user_id": user_obj.id, "message": message_text}
-        )       
+        mainapp_tasks.send_feedback_mail({"user_id": user_obj.id, "message": message_text})
         self.assertEqual(django_mail.outbox[0].body, message_text)
 
 
@@ -131,91 +129,75 @@ class TestCoursesWithMock(TestCase):
         "mainapp/fixtures/002_courses.json",
         "mainapp/fixtures/003_lessons.json",
         "mainapp/fixtures/004_teachers.json",
-            )
-    
+    )
+
     def test_page_open_detail(self):
         course_obj = mainapp_models.Courses.objects.get(pk=2)
         path = reverse("mainapp:courses_detail", args=[course_obj.pk])
-        with open(
-            "mainapp/fixtures/006_feedback_list_2.bin", "rb"
-                ) as inpf, mock.patch("django.core.cache.cache.get") as mocked_cache:
+        with open("mainapp/fixtures/006_feedback_list_2.bin", "rb") as inpf,    mock.patch(
+            "django.core.cache.cache.get"
+            ) as mocked_cache:
             mocked_cache.return_value = pickle.load(inpf)
-        result = self.client.get(path)
-        self.assertEqual(result.status_code, HTTPStatus.OK)
-        self.assertTrue(mocked_cache.called)
+            result = self.client.get(path)
+            self.assertEqual(result.status_code, HTTPStatus.OK)
+            self.assertTrue(mocked_cache.called)
 
 
 class TestNewsSelenium(StaticLiveServerTestCase):
     fixtures = (
         "authapp/fixtures/001_user_admin.json",
         "mainapp/fixtures/001_news.json",
-        )
-    
+    )
+
     browser = WebDriver.firefox_profile
-    browser.get('http://localhost')
-    
+    browser.get("http://localhost")
+
     def setUp(self):
         super().setUp()
-        self.selenium = WebDriver(
-        executable_path=settings.SELENIUM_DRIVER_PATH_FF
-            )
+        self.selenium = WebDriver(executable_path=settings.SELENIUM_DRIVER_PATH_FF)
         self.selenium.implicitly_wait(10)
         # Login
         self.selenium.get(f"{self.live_server_url}{reverse('authapp:login')}")
         button_enter = WebDriverWait(self.selenium, 5).until(
-            EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, '[type="submit"]')
-            )
+            EC.visibility_of_element_located((By.CSS_SELECTOR, '[type="submit"]'))
         )
         self.selenium.find_element_by_id("id_username").send_keys("admin")
         self.selenium.find_element_by_id("id_password").send_keys("admin")
         button_enter.click()
         # Wait for footer
-        WebDriverWait(self.selenium, 5).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, "mt-auto"))
-        )
+        WebDriverWait(self.selenium, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "mt-auto")))
 
     def test_create_button_clickable(self):
         path_list = f"{self.live_server_url}{reverse('mainapp:news')}"
         path_add = reverse("mainapp:news_create")
         self.selenium.get(path_list)
         button_create = WebDriverWait(self.selenium, 5).until(
-            EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, f'[href="{path_add}"]')
-            )
+            EC.visibility_of_element_located((By.CSS_SELECTOR, f'[href="{path_add}"]'))
         )
         print("Trying to click button ...")
-        button_create.click() # Test that button clickable
-        WebDriverWait(self.selenium, 5).until(
-            EC.visibility_of_element_located((By.ID, "id_title"))
-        )
+        button_create.click()  # Test that button clickable
+        WebDriverWait(self.selenium, 5).until(EC.visibility_of_element_located((By.ID, "id_title")))
         print("Button clickable!")
-        
-        
+
     # With no element - test will be failed
     # WebDriverWait(self.selenium, 5).until(
     # EC.visibility_of_element_located((By.ID, "id_title111))
     # )
-    
-    
+
     def test_pick_color(self):
         path = f"{self.live_server_url}{reverse('mainapp:main_page')}"
         self.selenium.get(path)
-        navbar_el = WebDriverWait(self.selenium, 5).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, "navbar"))
-        )
+        navbar_el = WebDriverWait(self.selenium, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "navbar")))
         try:
             self.assertEqual(
                 navbar_el.value_of_css_property("background-color"),
                 "rgb(255, 255, 155)",
             )
         except AssertionError:
-            with open(
-                "var/screenshots/001_navbar_el_scrnsht.png", "wb"
-            ) as outf:
+            with open("var/screenshots/001_navbar_el_scrnsht.png", "wb") as outf:
                 outf.write(navbar_el.screenshot_as_png)
             raise
-        
+
     def tearDown(self):
         # Close browser
         self.selenium.quit()
